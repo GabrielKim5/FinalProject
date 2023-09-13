f7152de658e7eb6975f2cb7ed5b5027911c03597
Gabe <gabecoolkim@yahoo.com>
2023-09-12 20:09:17 -0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/images", express.static("public/images"));
 
 /* ROUTES */
 app.use("/auth", authRouter);
